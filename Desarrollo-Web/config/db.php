26d103db7677082169985e4020597dc64dfed24a
JuanchiManfre <104739186+JUANCH1I@users.noreply.github.com>
2023-05-08 00:27:05 -0300

    proyecto

--- Desarrollo-Web/config/db.php
+++ Desarrollo-Web/config/db.php
@@ -1,13 +1,13 @@
 <?php
 
-define("DB_HOST", "190.228.29.62");
-define("DB_NAME", "bdwebet29");
-define("DB_USER", "bdwebet29");
-define("DB_PASS", "Tecnica29!");
+define("DB_HOST", "");
+define("DB_NAME", "");
+define("DB_USER", "");
+define("DB_PASS", "!");
 
 function conexion() {
    try {
-       $con = new PDO('mysql:host=190.228.29.62;dbname=bdwebet29', DB_USER, DB_PASS);
+       $con = new PDO('mysql:host=;dbname=', DB_USER, DB_PASS);
        return $con;
    } catch (PDOException $e) {
        echo "Error de conexión: " . $e->getMessage();
@@ -16,10 +16,10 @@ function conexion() {
 }
 return [
     'db' => [
-      'host' => '190.228.29.62',
-      'user' => 'bdwebet29',
-      'pass' => 'Tecnica29!',
-      'name' => 'bdwebet29',
+      'host' => '',
+      'user' => '',
+      'pass' => '',
+      'name' => '',
       'options' => [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
       ]
