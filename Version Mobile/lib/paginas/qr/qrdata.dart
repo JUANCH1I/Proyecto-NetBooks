9ecf425e63038017a6af9f5856e3994885e33b4b
Caro <carolina.gonzalez.437@alu.tecnica29de6.edu.ar>
2023-06-02 11:15:26 -0300

    Cambio a Presma

--- Version Mobile/lib/paginas/qr/qrdata.dart
+++ Version Mobile/lib/paginas/qr/qrdata.dart
@@ -1,7 +1,11 @@
 import 'package:mobile_scanner/mobile_scanner.dart';
 
 class QrData{
+  late int idmaterial;
+  late DateTime prestamo;
+  late DateTime prestamoextendido;
 
+  QrData({required this.idmaterial,required this.prestamo,required this.prestamoextendido});
 }
   
 
