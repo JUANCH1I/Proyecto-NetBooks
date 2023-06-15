9c4e4803f4f05a5147561d130cdba654a31b4332
JUANCH1I <juanimanfredi@gmail.com>
2023-06-15 02:30:47 -0300

    proyecto

--- Desarrollo-Web/abm/netbook/abrirqr.php
+++ Desarrollo-Web/abm/netbook/abrirqr.php
@@ -1,5 +1,6 @@
 <?php
-$nombreImagen = 'qrcodes/miimagen.png'; // Ruta relativa a la imagen
+$nombre = $_GET['nombre'];
+$nombreImagen = 'qrcodes/qrcode_' . $nombre . '.png'; // Ruta relativa a la imagen
 
 header('Content-Type: image/png'); // Establecer el tipo de contenido como imagen/png
 
