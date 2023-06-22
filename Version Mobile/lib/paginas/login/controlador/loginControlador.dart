b934178256144ad0d672550c602c960f1d3acd6b
Caro <carolina.gonzalez.437@alu.tecnica29de6.edu.ar>
2023-06-22 01:14:38 -0300

    actualizacion controladores

--- Version Mobile/lib/paginas/login/controlador/loginControlador.dart
+++ Version Mobile/lib/paginas/login/controlador/loginControlador.dart
@@ -3,30 +3,26 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 class loginControlador{
 
-Future<String> login(loginRequestModelo request) async {
-    final response = await http.post(Uri.parse("http://192.168.0.71/Presma/login.php"), body: request.toJson());
-    if (response.statusCode == 200){
-      String respuesta = json.decode(json.encode(response.body));
-      return respuesta;
-    }
-    else{
-      throw Exception("Error");
-    }
-  }
 
-  /* Future<loginRespuestaModelo> login(loginRequestModelo request)async{
+ List<loginRespuestaModelo> loginRespuestaFromJson(String jsonstring){
+  final data = jsonDecode(jsonstring);
+  return List<loginRespuestaModelo>.from(
+    data.map((item) => loginRespuestaModelo.fromJson(item))
+  );
+}
+
+  Future<List<loginRespuestaModelo>> loginRespuesta(loginRequestModelo request)async{
     
     final response = await http.post(Uri.parse('http://192.168.0.71/Presma/login.php'),
     body: request.toJson());
     if (response.statusCode == 200){
       
-    final respuesta = loginRespuestaModelo.fromJson(json.decode(json.encode(response.body)));
+  
+    List<loginRespuestaModelo> respuesta = loginRespuestaFromJson(response.body);
     return respuesta;
-
-
     }
     else{
-      throw Exception('Error');
+      return <loginRespuestaModelo>[];
     }
-  }*/
+  }
 }
